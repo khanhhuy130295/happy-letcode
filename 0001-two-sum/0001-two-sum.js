ac1d88e8821ec021e141575f1714a4ec6f31419a
Huy-dhk <26412958+khanhhuy130295@users.noreply.github.com>
2024-11-14 15:52:44 +0700

    Time: 4 ms (59.46%), Space: 51.9 MB (13.70%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    if (nums.length < 2 || nums.length > 10 ** 4) return [];
+	if (target < -(10 ** 9) || target > 10 ** 9) return [];
+    let map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    return [];
+};
