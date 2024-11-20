570d61562338f4884b515c219c7d1e8f77ec5087
Huy-dhk <26412958+khanhhuy130295@users.noreply.github.com>
2024-11-20 22:28:55 +0700

    Time: 43 ms (90.00%), Space: 58 MB (61.90%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.js
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.js
@@ -19,16 +19,16 @@ var takeCharacters = function (s, k) {
 
     let minutes = 0;
     let right = 0;
-    let extra_count = { a: count.a - k, b: count.b - k, c: count.c - k };
+    let remain_count = { a: count.a - k, b: count.b - k, c: count.c - k };
     for (let left = 0; left < n; left++) {
-        if (s[left] === 'a') extra_count.a--;
-        if (s[left] === 'b') extra_count.b--;
-        if (s[left] === 'c') extra_count.c--;
+        if (s[left] === 'a') remain_count.a--;
+        if (s[left] === 'b') remain_count.b--;
+        if (s[left] === 'c') remain_count.c--;
 
-        while (extra_count.a < 0 || extra_count.b < 0 || extra_count.c < 0) {
-            if (s[right] === 'a') extra_count.a++;
-            if (s[right] === 'b') extra_count.b++;
-            if (s[right] === 'c') extra_count.c++;
+        while (remain_count.a < 0 || remain_count.b < 0 || remain_count.c < 0) {
+            if (s[right] === 'a') remain_count.a++;
+            if (s[right] === 'b') remain_count.b++;
+            if (s[right] === 'c') remain_count.c++;
             right++;
         }
         minutes = Math.max(minutes, left - right + 1);
